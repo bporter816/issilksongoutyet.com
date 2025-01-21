91797e145b63cbf7d7c4fa5cb9de2cb3acfd0141
Benjamin Porter <bporter816@gmail.com>
2025-01-20 23:17:42 -0500

    Use Python because Cloudflare Pages doesn't support jq

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 mkdir -p _data
-curl 'https://store.steampowered.com/api/appdetails?appids=1030300' | jq '.["1030300"].data.release_date' > _data/steam.json
+curl 'https://store.steampowered.com/api/appdetails?appids=1030300' | python3 -c 'import json, sys; json.dump(json.load(sys.stdin)["1030300"]["data"]["release_date"], sys.stdout)' > _data/steam.json
 jekyll build
